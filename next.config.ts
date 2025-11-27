4beb666875566ec50014c55e83ed763dc4a8e9a0
dev-ashir768 <techashir167@gmail.com>
2025-11-28 00:35:10 +0500

    add bun in start cmd

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-ignore
-    turbo: false,
-    // @ts-ignore
-    turbopack: false,
-  },
-  swcMinify: true,
+
 };
 
 export default nextConfig;
