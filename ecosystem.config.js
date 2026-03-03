b4f9666aa3bf64d6eb64633a469e0490a673e493
Ashir Arif <techashir167@gmail.com>
2026-03-03 13:17:40 +0500

    new changes

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "oms-bot-frontend",
+      script: "bun",
+      args: "run next start -p 3000",
+      exec_mode: "fork",
+      instances: "1",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      out_file: "./logs/out.log",
+      error_file: "./logs/error.log",
+      merge_logs: true,
+    },
+  ],
+};
